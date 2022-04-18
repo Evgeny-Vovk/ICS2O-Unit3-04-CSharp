ac98c5b9e90b0a6f2dd9645107a9be3c956e1e88
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-04-18 23:17:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,29 @@
+// Created by: Evgeny
+// Created on: Mar 2022
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //input
+        float fahrenheit;
+
+        Console.WriteLine("This program calculates celsius out of fahrenheit.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the fahrenheit(°F): ");
+        fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+        //process
+        double celsius = (fahrenheit - 32) * 5/9;
+
+        //output
+        Console.WriteLine("");
+        Console.WriteLine(fahrenheit + " fahrenheit is " + celsius.ToString("0.00") + " celsius.");
+
+        Console.WriteLine("\nDone.");
+    }
+}
